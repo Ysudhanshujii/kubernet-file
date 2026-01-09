de4cacef54d364a3f4e96e65ec1a56c2c802e464
Ysudhanshujii <sudhanshuyadav202545@gmail.com>
2026-01-09 17:44:06 +0530

    Add Kubernetes deployment, service, and Dockerfile

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        this is the demo project for docker 
         </p>
         <a
           className="App-link"
@@ -21,5 +21,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
